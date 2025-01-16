a750483cde558ec7f963803f8b9e5e01057dc37d
BinishB123 <binishb2018@gmail.com>
2025-01-16 10:18:51 +0530

    changed api url;

--- src/api/common.tsx
+++ src/api/common.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiUrl = 'http://localhost:3000'
+export const apiUrl = 'api.binish.site'
 export const axiosInstance = axios.create({
   baseURL: apiUrl,
   withCredentials: true,                
